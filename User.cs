1743fc543875ecbd26d01c135b400f9719a71851
pryadavalli <21180429+pryadavalli@users.noreply.github.com>
2019-12-24 15:10:50 -0500

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServiceToken.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
